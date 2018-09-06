e98e19ce32f18d0727f97d4e7d3e5339b43a0a48
kimakuh <kimakuh@gmail.com>
2018-09-06 23:53:17 +0900

    .

--- react/Modern-React-with-Redux/ReduxSimpleStarter/src/components/search_bar.js
+++ react/Modern-React-with-Redux/ReduxSimpleStarter/src/components/search_bar.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
+// == const Component = React.Component;
 
-const SearchBar = () => {
-    return <input />;
-};
+
+// const SearchBar = () => {
+//     return <input />;
+// };
+
+
+
+
+class SearchBar extends Component { 
+// SearchBar라고 불리는 클래스를 생성하고,
+// React.Component로부터 모든 기능을 제공 받아라
+// => 이 검색바는 React.Component 클래스에게 받은 기능을 제공
+ render() { // 클래스에 메소드를 할당하는 구문
+    return <input onChange={event => console.log(event.target.value)} />;
+ }
+
+}
+
+
 
 export default SearchBar;
