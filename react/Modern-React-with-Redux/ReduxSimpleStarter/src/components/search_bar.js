1e8d436b9a761f0c86fae8b0042ffc58f1c4c0e1
kimakuh <kimakuh@gmail.com>
2018-09-07 15:52:27 +0900

    .

--- react/Modern-React-with-Redux/ReduxSimpleStarter/src/components/search_bar.js
+++ react/Modern-React-with-Redux/ReduxSimpleStarter/src/components/search_bar.js
@@ -13,8 +13,24 @@ class SearchBar extends Component {
 // SearchBar라고 불리는 클래스를 생성하고,
 // React.Component로부터 모든 기능을 제공 받아라
 // => 이 검색바는 React.Component 클래스에게 받은 기능을 제공
- render() { // 클래스에 메소드를 할당하는 구문
-    return <input onChange={event => console.log(event.target.value)} />;
+ 
+ constructor(props) {
+     super(props);
+
+     this.state = { term : 'Starting Value' }
+ }
+
+ // 스테이트를 정의하거나 재초기화하는 방법
+ // 클래스기반 컴포넌트만 스테이트를 가질 수 있음
+
+render() { // 클래스에 메소드를 할당하는 구문
+    return (
+        <div>
+            <input 
+            value={this.state.term}
+            onChange={event => this.setState({ term : event.target.value })} />
+        </div>
+    );
  }
 
 }
