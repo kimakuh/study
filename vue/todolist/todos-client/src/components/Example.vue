304430f37b9217f225184cb87a603cbe0b14135a
kimakuh <kimakuh@gmail.com>
2018-10-31 14:28:40 +0900

    .

---
+++ vue/todolist/todos-client/src/components/Example.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="panel panel-default">
+        <div class="panel-heading">Panel heading widthout title</div>
+        <div class="panel-body">
+            panel content
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Example',
+    data () {
+        return {
+            msg: 'Basic panel example'
+        }
+    }
+}
+</script>
--- w3schools/README.md
+++
@@ -1,4 +0,0 @@
-# w3cschools
-
-개인 공부
-w3cschools 따라 해보기!
