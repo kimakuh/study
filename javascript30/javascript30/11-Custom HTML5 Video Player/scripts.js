8d1a1272688563898877b9d5aad5f0e0ed6c026a
kimakuh <kimakuh@gmail.com>
2019-04-17 10:51:28 +0900

    .

---
+++ javascript30/javascript30/11-Custom HTML5 Video Player/scripts.js
@@ -0,0 +1,74 @@
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+
+
+
+function togglePlay() {
+  const method = video.paused ? 'play' : 'pause';
+  video[method]();
+  // if(video.paused) {
+  //   video.play();
+  // } else {
+  //   video.pause();
+  // }
+}
+
+function updateButton() {
+  const icon = this.paused ? '►' : '∥';
+  toggle.textContent = icon;
+  console.log('Update the button');
+}
+
+
+function skip() {
+  console.log('Skipping!');
+  console.log(this.dataset);
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
+
+function handleRangeUpdate() {
+  video[this.name] = this.value;
+  console.log(this.value);
+  console.log(this.name);
+}
+
+
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.getElementsByClassName.flexBasis = `${percent}%`;
+}
+
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+  console.log(e);
+
+}
+
+
+
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
+toggle.addEventListener('click', togglePlay);
+skipButtons.forEach(button => button.addEventListener(
+  'click', skip));
+ranges.forEach(button => button.addEventListener(
+  'change', handleRangeUpdate));
+ranges.forEach(button => button.addEventListener(
+  'mousemove', handleRangeUpdate));
+
+
+let mousedown = false;
+
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
---
+++ javascript30/javascript30/19-Unreal Webcam Fun/scripts.js
@@ -0,0 +1,33 @@
+const video = document.querySelector('.player');
+const canvas = document.querySelector('.photo');
+const ctx = canvas.getContext('2d');
+const strip = document.querySelector('.strip');
+const snap = document.querySelector('.snap');
+
+
+function getVideo() {
+  navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: false
+    })
+    .then(localMediaStrem => {
+      console.log(localMediaStrem);
+      video.src = window.URL.createObjectURL(localMediaStrem);
+      video.play();
+    })
+    .catch(err => {
+      console.error(`OH NO!!`, err);
+    });
+}
+
+
+function paintToCanvas() {
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+  console.log(width, height);
+  canvas.width = width;
+  canvas.height = height;
+}
+
+
+getVideo();
